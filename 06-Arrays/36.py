be35703ce084295a86976629abbda2d02cc4e557
Adison Cavani <adriansrodon@tuta.io>
2023-12-02 12:27:32 +0100

    feat: add 06-arrays

---
+++ 06-Arrays/36.py
@@ -0,0 +1,6 @@
+var = (50, 20, 40, 50, 30, 50)
+value = 50
+
+print(f"Tuple: {var}")
+print(f"Value: {value}")
+print(f"Number of occurrences: {var.count(value)}")
---
+++ 06-Arrays/37.py
@@ -0,0 +1,10 @@
+import m_37_MyArrays as fn
+
+arr = [7, 3, 8, 5, 2]
+
+print(f"Numbers: {arr}")
+print(f"Second largest number: {fn.second_largest(arr)}")
+print(f"Diff: {fn.diff_min_max(arr)}")
+print(f"Median: {(fn.median(arr))}")
+print(f"Smallest and largest number: {fn.min_max(arr)}")
+print(f"Numbers as a string: {fn.array_str(arr)}")
